d29f8f1e117778d573553eeb71e7819d70bd7f98
kenshen112 <finalfantasytimothy@gmail.com>
2024-01-30 09:29:49 -0700

    Small fixes, Added NPC

---
+++ Source/Npc/Npc.cs
@@ -0,0 +1,22 @@
+using Godot;
+using System;
+
+namespace RPG;
+
+public partial class Npc : Node2D
+{
+    int id;
+    string NpcName;
+    Sprite2D NpcSprite;
+    Graph<DialogueNode> Conversation;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+    }
+}
