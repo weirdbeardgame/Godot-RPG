99e47e8e584d93bd10f90a4d8137c1c82dee5ef6
darshlimb <darshlimb123@gmail.com>
2025-04-12 02:52:23 +0530

    dsfksdfk

--- Source/GameUIScenes/Hud.cs
+++ Source/GameUIScenes/Hud.cs
@@ -1,23 +1,43 @@
 using Godot;
 using System;
+using System.Diagnostics;
 using System.Xml.Resolvers;
 
 public partial class Hud : CanvasLayer
 {
+    // The idea is that we provide the implementation of the Hud System and let devs either choose one
+    // of our premade UI scenes or pop in and out their own handcrafted UI Scenes right from the editor
+
     [ExportGroup("UI Scenes Directories")]
+    [Export] PackedScene PlayerStatUIScene;
+    [Export] PackedScene PartyStatUIScene;
+    [Export] PackedScene MiniMapUIScene;
     [Export] PackedScene QuestUIScene;
 
+    // These Control Nodes are made to determine the size and position of their respective UI scene both
+    // at ready as well as runtime
+    
     [ExportGroup("UI Markers")]
+    [Export] Control MarkerPlayerStatUI;
+    [Export] Control MarkerPartyStatUI;
+    [Export] Control MarkerMiniMapUI;
     [Export] Control MarkerQuestUI;
-    [Export] Godot.Collections.Array<Panel> Previews;
+    [Export] Godot.Collections.Array<Panel> Previews; // Just for showcasing the child UI's size and position
 
     public override void _Ready()
     {
         HidePreviewPanels();
         
+        SetupPlayerStatUI();
+        SetupPartyStatUI();
+        SetupMiniMapUI();
         SetupQuestUI();
     }
 
+
+    private void SetupPlayerStatUI() { Debug.Print("Showing Player Stats"); }
+    private void SetupPartyStatUI() { Debug.Print("Showing Party Stats"); }
+    private void SetupMiniMapUI() { Debug.Print("Showing MiniMap"); }
     private void SetupQuestUI()
     {
         if (QuestUIScene != null)
@@ -32,6 +52,7 @@ public partial class Hud : CanvasLayer
         foreach(Panel panel in Previews)
         {
             panel.Hide();
+            // probably should queuefree() instead
         } 
     }
 }
--- Source/GameUIScenes/MainQuestUi.cs
+++ Source/GameUIScenes/MainQuestUi.cs
@@ -7,34 +7,52 @@ public partial class MainQuestUi : Control
 {
     [ExportGroup("Node Injection")]
     [Export] private Control QuestHeaderControl;
-    [Export] private Control QuestDetailControl;
-    [Export] private Panel QuestBGPanel;
+    [Export] private Control QuestConditionsControl;
+    [Export] private Panel BGPanel;
+    [Export] private Control ConditionTextControl;
 
     [ExportGroup("Label Injection")]
     [Export] private Label TitleLabel;
     [Export] private Label QuestDescriptionLabel;
-    [Export] private Label QuestConditionLabel;
+    //[Export] private Label QuestConditionLabel;
 
     [ExportGroup("Size Manipulation")]
     [Export] private float HeaderYSize = 200f;
+    [Export] private float ConditionDetailYSize = 1.0f;
     [Export] private float Spacing = 40f;
 
     public override void _Ready()
     {
-        UpdateQuestUIPosition();
         UpdateQuestText();
+        AddConditionTexts(3);
+        UpdateQuestUIPosition();
     }
 
-    private void UpdateQuestText() { TitleLabel.Text = "Active Quest Title"; }
+    private void UpdateQuestText() 
+    { 
+        if (TitleLabel != null) { TitleLabel.Text = "Tracked Quest Title"; }
+        if (QuestDescriptionLabel != null) { QuestDescriptionLabel.Text = "Tracked Quest Description"; }
+    }
 
-    private void UpdateQuestUIPosition() { QuestDetailControl.Position = new Vector2(QuestHeaderControl.Position.X, QuestHeaderControl.Position.Y + (QuestHeaderControl.Size.Y * QuestHeaderControl.Scale.Y) + Spacing); }
+    private void UpdateQuestUIPosition() 
+    { 
+        QuestConditionsControl.Size = new Vector2(QuestHeaderControl.Size.X, ConditionDetailYSize * (QuestHeaderControl.Size.Y * QuestHeaderControl.Scale.Y));
+        QuestConditionsControl.Position = new Vector2(QuestHeaderControl.Position.X, QuestHeaderControl.Position.Y + (QuestHeaderControl.Size.Y * QuestHeaderControl.Scale.Y) + Spacing); 
+    }
 
-    private void AddConditionLabels(int totalNumber)
+    private void AddConditionTexts(int totalNumber)
     {
+        float accumulatedSize = 0;
         for (int i = 0; i < totalNumber; i++)
         {
-            Label ConditionLabel = new Label();
-            AddChild(ConditionLabel);
+            Control newConditionText = (Control)ConditionTextControl.Duplicate();
+            QuestConditionsControl.AddChild(newConditionText);
+
+            newConditionText.Visible = true;
+            newConditionText.Size = new Vector2(QuestHeaderControl.Size.X, ConditionDetailYSize * (QuestHeaderControl.Size.Y * QuestHeaderControl.Scale.Y));
+            newConditionText.Position = new Vector2(0,accumulatedSize);
+
+            accumulatedSize += newConditionText.Size.Y;
         }
     }
 }
