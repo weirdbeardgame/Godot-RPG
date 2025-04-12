dd42ac9221b194500be97deb12f980c58b6ac7db
darshlimb <darshlimb123@gmail.com>
2025-04-12 13:47:38 +0530

    Moved Files and added PlayerStatUI scene

--- Assets/GameUIScenes/Hud.cs
+++ Assets/GameUIScenes/Hud.cs
@@ -6,6 +6,7 @@ namespace Core.UI;
 
 public partial class Hud : CanvasLayer
 {
+    private bool hudInitialized = false;
     // The idea is that we provide the implementation of the Hud System and let devs either choose one
     // of our premade UI scenes or pop in and out their own handcrafted UI Scenes right from the editor
 
@@ -23,20 +24,33 @@ public partial class Hud : CanvasLayer
     [Export] Control MarkerPartyStatUI;
     [Export] Control MarkerMiniMapUI;
     [Export] Control MarkerQuestUI;
-    [Export] Godot.Collections.Array<Panel> Previews; // Just for showcasing the child UI's size and position
+    [Export] Godot.Collections.Array<Control> Previews; // Just for showcasing the child UI's size and position
 
     public override void _Ready()
     {
+        if (hudInitialized) { return; }
+
         HidePreviewPanels();
-        
+        InitializeHud();
+    }
+
+    private void InitializeHud()
+    {
         SetupPlayerStatUI();
         SetupPartyStatUI();
         SetupMiniMapUI();
         SetupQuestUI();
     }
 
-
-    private void SetupPlayerStatUI() { Debug.Print("Showing Player Stats"); }
+    private void SetupPlayerStatUI() 
+    { 
+        if (PlayerStatUIScene != null)
+        {
+            Control playerUiInstance = (Control)PlayerStatUIScene.Instantiate();
+            MarkerPlayerStatUI.AddChild(playerUiInstance);
+            // playerUiInstance.UpdateUI();
+        } 
+    }
     private void SetupPartyStatUI() { Debug.Print("Showing Party Stats"); }
     private void SetupMiniMapUI() { Debug.Print("Showing MiniMap"); }
     private void SetupQuestUI()
