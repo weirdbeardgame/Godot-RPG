327ef58f004d3e6494cf7a4942c0b0f99c2fdbb6
weirdbeardgame <obarrtimothy@gmail.com>
2025-04-13 14:50:35 -0700

    WIP: Work on fleshing out player mechanics

--- Source/Player/Party.cs
+++ Source/Player/Party.cs
@@ -1,28 +1,43 @@
-
+using Core.Items;
 
 public partial class Party : Node
 {
-    public List<Player> PlayerParty;
+    [Export] private Godot.Collections.Array<Player> _playerParty;
+
+    private Inventory _playerInventory;
+    public void RecieveSingleItem(Item it) => _playerInventory.Add(new List<Item> { it });
+    public void RecieveMultipleItems(List<Item> items) => _playerInventory.Add(items);
+
+    Player GetPlayer(int ID) => _playerParty[ID];
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         // Read from data files to create and init player Party
-        PlayerParty = new();
-
+        _playerParty = new();
     }
 
+    // InGame party add
     public bool Add(Player p)
     {
+        if (!_playerParty.Contains(p))
+        {
+            _playerParty.Add(p);
+            return true;
+        }
 
+        GD.Print("Party already contains player");
         return false;
     }
 
-    Player GetPlayer(int pID) => PlayerParty[pID];
-
+    // InGame party remove. Some character was a temp or was taken out for some story reason
     public void Remove(Player p)
     {
+        if (_playerParty.Contains(p))
+        {
+            _playerParty.Remove(p);
+        }
     }
-
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta)
--- Source/Player/Player.cs
+++ Source/Player/Player.cs
@@ -3,49 +3,39 @@ using System.Collections;
 using Core.Items;
 using Godot;
 
-
-
 public partial class Player : Creature
 {
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+    public void Equip(Equipable e)
+    {
+        foreach (var slot in _slots)
+        {
+            if (slot.CanBeEquipped(e, e.Job))
+            {
+                slot.Equip(e, e.Job);
+            }
+        }
+    }
 
-	public void Equip(Equipable e)
-	{
-		foreach (var slot in _slots)
-		{
-			if (slot.CanBeEquipped(e, e.Job))
-			{
-				slot.Equip(e, e.Job);
-			}
-		}
-	}
+    public void Die()
+    {
+        if (_stats["Health"].Stat == 0)
+        {
+            IsAlive = LivingStatus.DEAD;
+        }
+    }
 
-	public void Die()
-	{
+    public void LevelUp()
+    {
+        // Just some filler code, this should be more complicated
+        if (Experience >= ExpReqForLevelUp)
+        {
 
-	}
+        }
+    }
 
-	public void LevelUp()
-	{
-
-	}
-
-	public void RecieveItem(Item it)
-	{
-
-	}
-
-	public void UseItem(Item it)
-	{
-
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+    }
 }
